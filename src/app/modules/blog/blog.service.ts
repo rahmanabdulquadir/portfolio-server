7c4870b248ff65649348e81b424a5a825e0a24a8
rahmanabdulquadir <rahmanaq777@gmail.com>
2024-10-22 16:17:32 +0600

    blog services added

---
+++ src/app/modules/blog/blog.service.ts
@@ -0,0 +1,24 @@
+import { Blog } from "./blog.model"
+import { IBlog } from "./blog.router"
+
+const createBlogIntoDB=async(payload:IBlog)=>{
+  const result=await Blog.create(payload)
+
+  return result
+}
+const getBlogFromDB=async()=>{
+  const result=await Blog.find()
+
+  return result
+}
+const getSingleBlogFromDB=async(id:string)=>{
+  const result=await Blog.findById(id)
+
+  return result
+}
+
+export const blogServices={
+  createBlogIntoDB,
+  getBlogFromDB,
+  getSingleBlogFromDB
+}
