d5a5ab3ed875e999b2ec661f4b5797b71b5b5454
rahmanabdulquadir <rahmanaq777@gmail.com>
2024-10-22 16:14:26 +0600

    blog module created

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(cors());
 
 // application routes
-app.use("/api", router);
+app.use("/api/v1", router);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
---
+++ src/app/modules/blog/blog.router.ts
@@ -0,0 +1,5 @@
+export interface IBlog {
+  title: string;
+  image: string;
+  description: string;
+}
